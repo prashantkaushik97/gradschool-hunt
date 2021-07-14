ebc60cdc7511d13de0e2a7cfc00a84123f2fdff6
prashantkaushik97 <kaushik.prashant00@gmail.com>
2021-07-14 22:04:49 +0530

    Dropdown

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Jumbotron } from "react-bootstrap";
 import Layout from "../components/Layout";
 import Select from "react-select";
 import UniversitySearch from "./UniversitySearch";
-function onChangeInput(value){
-  console.log(value.label)
-}
+
+
 function Home() {
+  const [university, setUniversity] = useState(null);
+  function onChangeInput(value) {
+  }
   return (
     <Layout>
       <Jumbotron className="text-center">
-        <UniversitySearch onChange ={onChangeInput} label="Choose a university"></UniversitySearch>
+        <UniversitySearch onChange={(value) => {
+          setUniversity(value.label)
+        }} label="Choose a university"></UniversitySearch>
+        <button onClick={()=>console.log(university)}>Pressme</button>
       </Jumbotron>
     </Layout>
   );
